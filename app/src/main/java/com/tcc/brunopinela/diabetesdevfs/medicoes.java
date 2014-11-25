6f962789521f40a6e058d6a677a032682af37084
pineladsn <brunopinela.alpes@gmail.com>
2014-11-25 21:18:52 -0200

    alteracoes teste

--- app/src/main/java/com/tcc/brunopinela/diabetesdevfs/medicoes.java
+++ app/src/main/java/com/tcc/brunopinela/diabetesdevfs/medicoes.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,6 +25,7 @@ public class medicoes extends Activity {
     SQLiteDatabase bancoDados = null;
     Cursor cursor;
     private EditText editDia, editHora, editGlicose, editTipoInsulina, editUnidades, editPeriodo, editObs;
+    Button ButtonDay = (Button) findViewById(R.id.button1);
     private DatePicker datePicker;
     private Calendar calendar;
     private TextView dateView;
@@ -63,19 +65,16 @@ public class medicoes extends Activity {
     private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener()
     {
         @Override
-        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3)
+        public void onDateSet(DatePicker view,int year,int monthOfYear,int dayOfMonth)
         {
-            // TODO Auto-generated method stub
-            // arg1 = year
-            // arg2 = month
-            // arg3 = day
+            calendar.set(year,monthOfYear,dayOfMonth);
         }
     };
 
     public void inicializacaoObjetos()
     {
         try{
-            editDia =
+            //editDia = myDateListener.onDateSet(dateView, year, month, day);
             editGlicose = (EditText) findViewById(R.id.txtGlicemia);
             editTipoInsulina = (EditText) findViewById(R.id.editText2);
             editUnidades = (EditText) findViewById(R.id.editText3);
